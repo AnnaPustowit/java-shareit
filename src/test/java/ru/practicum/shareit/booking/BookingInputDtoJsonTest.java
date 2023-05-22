d09d2999957c816ec03c76128981aee3e4e503d5
AnnaPustowit <pustowitanna@yandex.ru>
2023-05-23 01:52:14 +0300

    Исправление тестовых методов 3

--- src/test/java/ru/practicum/shareit/booking/BookingInputDtoJsonTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingInputDtoJsonTest.java
@@ -1,2 +1,29 @@
-package ru.practicum.shareit.booking;public class BookingInputDtoJsonTest {
+package ru.practicum.shareit.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import ru.practicum.shareit.booking.dto.BookingInputDto;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@JsonTest
+public class BookingInputDtoJsonTest {
+    @Autowired
+    private JacksonTester<BookingInputDto> json;
+
+    @Test
+    void testJsonBookingRequestDto() throws IOException {
+        String jsonString = "{\"itemId\":\"1\", \"start\":\"2023-05-22T18:00:00\", \"end\":\"2023-05-23T18:00:00\"}";
+
+        BookingInputDto bookingInputDto = this.json.parse(jsonString).getObject();
+
+        assertThat(bookingInputDto.getItemId()).isEqualTo(1L);
+        assertThat(bookingInputDto.getStart()).isEqualTo(LocalDateTime.of(2023, 5, 22, 18, 0, 0));
+        assertThat(bookingInputDto.getEnd()).isEqualTo(LocalDateTime.of(2023, 5, 23, 18, 0, 0));
+    }
 }
--- src/test/java/ru/practicum/shareit/item/CommentDtoJsonTest.java
+++ src/test/java/ru/practicum/shareit/item/CommentDtoJsonTest.java
@@ -1,2 +1,26 @@
-package ru.practicum.shareit.item;public class CommentDtoJsonTest {
+package ru.practicum.shareit.item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import ru.practicum.shareit.item.dto.CommentDto;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@JsonTest
+public class CommentDtoJsonTest {
+    @Autowired
+    private JacksonTester<CommentDto> json;
+
+    @Test
+    void testJsonCommentDto() throws IOException {
+        String jsonString = "{\"text\":\"Новый комментарий\"}";
+
+        CommentDto commentDto = this.json.parse(jsonString).getObject();
+
+        assertThat(commentDto.getText()).isEqualTo("Новый комментарий");
+    }
 }
